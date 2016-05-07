fe674c39dff6ca23582bee5e4337d3b750656b24
NhuTran <trandinhnhubk@gmail.com>
2016-05-07 13:19:13 +0700

    add function for user model

--- webapps/models/User_model.php
+++ webapps/models/User_model.php
@@ -1,5 +1,5 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Created by IntelliJ IDEA.
  * User: hoanvo
@@ -9,5 +9,53 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class User_Model extends CI_Model
 {
+    private static $TABLE_NAME = 'user';
 
+    public function authenticate($username, $password)
+    {
+        $this->db->from(self::$TABLE_NAME);
+        $this->db->where('username', $username);
+        $this->db->where('password', MD5($password));
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return $query->result();
+        }
+        return false;
+    }
+
+    public function findById($id)
+    {
+        $this->db->from('user');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function findAll()
+    {
+        $this->db->from(self::$TABLE_NAME);
+        $query = $this->db->get();
+        if ($query->num_rows() >= 1) {
+            return $query->result();
+        }
+        return false;
+    }
+
+    public function insert($value)
+    {
+        $this->db->insert(self::$TABLE_NAME, $value);
+    }
+
+    public function update($value, $id)
+    {
+        $this->db->where('id', $id);
+        $this->db->update(self::$TABLE_NAME, $value);
+    }
+
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete(self::$TABLE_NAME);
+    }
 }
